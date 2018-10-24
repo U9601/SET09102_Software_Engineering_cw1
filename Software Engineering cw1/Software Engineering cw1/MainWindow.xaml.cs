b7c043b7e3a33ae7afbbb73a07d06d78dce6a268
Chris Jones <ctjones1965@gmail.com>
2018-10-24 23:37:02 +0100

    added the basic structure of S E T

--- Software Engineering cw1/Software Engineering cw1/MainWindow.xaml.cs
+++ Software Engineering cw1/Software Engineering cw1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,39 @@ namespace Software_Engineering_cw1
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string messageID = textBox.Text;
+            string messageBody = textBlock.Text;
+            string senderID = textBox2.Text;
+
+            if (messageID[0].ToString().Equals("S"))
+            {  
+                if (IsPhoneNumber(senderID))
+                {
+                    MessageBox.Show("Phone number is valid");
+                }
+                else
+                {
+                    MessageBox.Show("Phone number is not valid");
+                }
+            }
+            else if (messageID[0].ToString().Equals("E"))
+            {
+                MessageBox.Show("E");
+            }
+            else if (messageID[0].ToString().Equals("T"))
+            {
+                MessageBox.Show("T");
+            }
+        }
+
+        public static bool IsPhoneNumber(string number)
+        {
+            return Regex.Match(number, @"^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$").Success;
+        }
+
+        
     }
 }
